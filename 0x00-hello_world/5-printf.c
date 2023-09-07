c706ac7776841650ef6919091e93366c228ad95f
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-07 16:00:08 -0700

    I added a comment above the main function and () around the 0 to avoid that thing

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* I wrote this comment and the add () to 0 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
