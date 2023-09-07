bbc1c31e9f1bc6b76451323f99b27bc05cce6948
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-07 16:45:20 -0700

    The 5th mandatory task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - my main function
+ * Return: The description of the (return) statement
+*/
+int main(void)
+{
+	char n[] = "Programming is like bulding a multilingual puzzle";
+	puts(n);
+	return (0);
+}
