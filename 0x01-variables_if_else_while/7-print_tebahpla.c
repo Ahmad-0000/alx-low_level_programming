5a43183e0a9eb90728771f429400db44bff5243f
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-08 16:03:27 -0700

    The solution of the 8th task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is main
+ * Return: is return
+*/
+int main(void)
+{
+	char a = 'z';
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
