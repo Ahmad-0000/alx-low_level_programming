979fd9d1f4ae06d2ab7322f1ee1f8b52fe30be0f
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-08 15:36:08 -0700

    The 5th mandatory task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - is main
+ * Return: is return
+*/
+int main(void)
+{
+	char a = 'a';
+	char A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e' || a =='q')
+		{
+		
+		}
+		else
+		{
+			A = a;
+			putchar(A);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
