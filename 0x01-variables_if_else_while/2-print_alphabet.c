c4da286349fdd2540129df4ab8dd27fe0ca6afdf
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-08 15:13:52 -0700

    The 3d mandatory task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is main
+ * Return: is return
+*/
+int main(void)
+{
+	char a = 'a';
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
