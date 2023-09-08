7e3f5842f2bf41a13bbc96c8fb3c83a89ac4bb93
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-08 16:40:20 -0700

    The last mandatory task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - is main
+ * Return: is return
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+		putchar( ',');
+	}
+	putchar('\n');
+	return (0);
+}
