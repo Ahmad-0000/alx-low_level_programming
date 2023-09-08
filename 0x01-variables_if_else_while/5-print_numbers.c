30974018cbe1ae596154a3564f09ab4f7a903c1b
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-08 15:43:34 -0700

    The 5th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is main
+ * Return: is return
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
