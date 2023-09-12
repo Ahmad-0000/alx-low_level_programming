0300d69507524026b51a3d71b393a8f9ac15b149
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-12 16:45:25 -0700

    I changed the solution of the 2nd task.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-void print_alphabet(void)
-{	
-	char a;
-
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,13 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 int _islower(int c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
