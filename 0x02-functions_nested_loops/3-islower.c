8616d00f746ff1d890982b8319843d63428bbc78
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-13 20:53:15 +0000

    I changed the solution of the 4th task.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,7 @@
 #include "main.h"
-
 int _islower(int c)
 {
-	int r = c;
-	_putchar(r + '0');
-	if (("%d", c) < 97  || ("%d", c) > 122)
+	if (c < 97 || c > 122)
 		return (0);
 	else
 		return (1);
