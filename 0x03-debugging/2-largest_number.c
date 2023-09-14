28b08208d2a18e1bd79b78c6bf9725a8ff0250e0
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-14 14:09:27 +0000

    Added some braces.

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,11 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a >= b && c =< a)
+	{
 		largest = a;
+	}
 	else if (b >= a && c =< b)
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
+	}
 	
 	return (largest);
 }
