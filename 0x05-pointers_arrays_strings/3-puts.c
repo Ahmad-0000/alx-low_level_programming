434c4fd00ca498a766aaa9ecea86451d2c22929e
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-19 08:05:27 +0000

    Added the solution of the 4th mandatory task.

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - is a function like (puts) function
+ * @str: is a pointer
+ */
+void _puts(char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
