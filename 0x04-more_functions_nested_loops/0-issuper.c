72e6d8b0d2c007d79557d7a92de2acf041b0e2cf
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 12:53:05 +0000

    The solution of the 1st mandatory task

--- 0x04-more_functions_nested_loops/0-issuper.c
+++ 0x04-more_functions_nested_loops/0-issuper.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _isupper - is a function to determine whether a letter is uppercase or no
+ * @c: is the letter to check
+ * Return: is to return 1 if uppercase or 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c < 65 || c > 90)
