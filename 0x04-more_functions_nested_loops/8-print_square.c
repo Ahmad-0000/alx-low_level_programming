de8101334da4b48e3dc885f55a0f8dd80a44d719
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 14:41:05 +0000

    The solution of the 9th mandatory task.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - is a function to print a square
+ * @size: is the width and the height
+ */
+void print_square(int size)
+{
+	int i, n;
+	
+	i = 0;
+	while (i <= size)
+	{
+		n = 0;
+		while (n <= size)
+		{
+			_putchar('#');
+			n++;
+		}
+		i++;
+		_putchar('#');
+	}
+}
