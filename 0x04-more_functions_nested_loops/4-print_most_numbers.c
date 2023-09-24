dc78a6123b0c5ae25ce32c4b32db7e892c393189
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 13:26:45 +0000

    The solution of the 5th mandatory task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - is a function to print 8 numbers
+ */
+void printing(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+			_putchar(i + 48);
+	}
+}
