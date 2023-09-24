5871c9ea779b11e8bd67304996fee39d385bd456
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 13:08:37 +0000

    The 3d mandatory task and some prototypes.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _isdigit - is a function
+ * @c: is the param to check
  * Return: is the function value
  */
-
 int _isdigit(int c)
 {
 	if (c < 48 || c > 57)
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.c"
+/**
+ * mul - is a function to multiply two numbers
+ * @a: is the 1st number
+ * @b: is the 2nd number
+ *
+ */
+int mul(int a,int b)
+{
+	int i = a * b;
+}
