bcb2a85125465ddbf2f78f8c509a288b13513761
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 20:17:20 +0000

    The solution of the 6th mandatory task.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - is a function to print a string in reverse
+ * @s: a pointer to the string
+ */
+void rev_string(char *s)
+{
+	int i, n, f;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	char *p, *tmp;
+
+	p = s;
+	n = 0;
+	while (s[n] != '\0')
+	{
+		tmp[n] = s[i];
+		n++;
+		i--;
+	}
+	f = 0;
+	while (tmp[f] != '\0')
+	{
+		s[f] = tmp[f];
+		f++;
+	}
+}
