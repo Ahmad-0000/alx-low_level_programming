055ceeeb744ebd6e5fd0e9eb5f3e5f928e8b839a
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 13:55:52 +0000

    Added some changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,9 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(i + 48);
-			i++;
-		}
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
