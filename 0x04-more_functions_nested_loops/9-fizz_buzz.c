1dfe62ce3a1502b34c2111d525364089c22e230d
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-24 15:42:12 +0000

    The solution of the 10th mandatory task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - is my main function to do the FizzBuzz program
+ * Return: is to return a value
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (3);
+}
