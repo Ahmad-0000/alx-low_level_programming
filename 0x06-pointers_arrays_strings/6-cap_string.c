a19b95e5eb1d2adfd59b4fb4d3f606df9fe4a40b
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-25 17:35:34 +0000

    The solution of the 7th mandatory task.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * cap_string - is to captialize the first letter from every word in a string
+ * @c: is a pointer to the string
+ * Return: is to return the product string.
+ */
+char *cap_string(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (i != 0 && c[i] >= 97 && c[i] <= 122 && c[i - 1] < 65
+		|| c[i - 1] > 90 && c[i - 1] < 97 || c[i - 1] > 122)
+		{
+			c[i] -= 32;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (c);
+}
