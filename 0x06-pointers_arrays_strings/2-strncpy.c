dec7e7ef052b1281658d9c61f420c057638b678d
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-25 12:54:46 +0000

    The solution of the 3th mandatory task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - is a function to copy a string
+ * @dest: is a pointer to a string that will be copied upon
+ * @src: is a pointer to a string that will be copied from
+ * @n: is the most number of characters to be copied
+ *
+ * Return: is to return a pointer to the product string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && dest[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
