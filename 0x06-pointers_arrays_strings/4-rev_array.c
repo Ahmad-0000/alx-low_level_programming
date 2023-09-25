b06e031de0a7946662f2ba36c04e7a914d064cc0
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-25 15:58:07 +0000

    Changed the solution

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,29 +7,20 @@
  */
 void reverse_array(int *a, int n)
 {
-	int b[n], i, n2, m, s;
+	int i = 0;
 
-	s = n;
-	m = n;
-	i = 0;
-	while (i < n)
+	n -= 1;
+	while (i <= n)
 	{
-		b[i] = a[m - 1];
-		i++;
-		m--;
-	}
-	n2 = 0;
-	while (n2 < s)
-	{
-		if (s - n2 == 1)
+		if (n == 0)
 		{
-			printf("%d", b[n2]);
-			n2++;
+			printf("%d", a[n]);
+			n--;
 		}
 		else
 		{
-			printf("%d, ", b[n2]);
-			n2++;
+			printf("%d, ", a[n]);
+			n--;
 		}
 	}
 	printf("\n");
