12ff7b9375aaf42904ea8b8e3da4fb4ed06d387c
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-25 05:59:26 +0000

    The solution of the 10th mandatory task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - is a functio to concatenate two strings
+ * @dest: is a pointer of the string to be concatenated to
+ * @src: is a pointer to the string to be concatenated
+ *
+ * Return: is to return a pointer to the product string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	n = 0;
+	while (dest[n] < src[i])
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	return (dest);
+}
