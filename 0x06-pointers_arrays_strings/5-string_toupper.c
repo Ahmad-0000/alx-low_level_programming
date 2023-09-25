070eaa0b656ad08c9f0c5c9c3421c2e88ba12890
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-25 16:58:15 +0000

    The solution of the 6th mandatory task.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - is a function to convert a string to uppercase
+ * @c: is a pointer to the string
+ * Return: is to return a pointer to the string after conversion
+ */
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			c[i] -= 32;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (c);
+}
