4ffecb13d451d97784a7c08e3b59561ea83be1a6
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-26 17:13:09 +0000

    The solution of the 1st mandatory task.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - is a function to a fill an array with a constant
+ * @s: is a pointer to the array to fill
+ * @b: is the constant character to fill with
+ * @n: is the number of the times (b) will be filled in the array
+ *
+ * Return: is to return a pinter to the manipulated array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
