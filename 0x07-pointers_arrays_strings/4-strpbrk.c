20b8fd337200c46b33ff0afffd1db8343bae1da3
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-27 01:00:28 +0000

    The last thing in month 0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,47 +1,20 @@
 #include "main.h"
-#include <stdio.h>
-
-int main()
-{
-	char a[] = "hello world";
-	char b[] = "world";
-
-	_strpbrk(a, b);
-}
+/**
+ * _strpbrk - i
+ * @s: a
+ * @accept: is an accept
+ *
+ * Return: is a return
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	int j, n;
+	int i, n;
 
-	j = 0;
-	while (s[j] != '\0')
-	{
-		n = 0;
-		while (accept[n] != '\0')
-		{
-			if (s[j] == accept[n])
-			{
-				s[j + 1] = '\0';
-				break;
-			}
-			else
-			{
-				n++;
-			}
-		}
-		printf("j is %d\n", j);	
-		j++;
-	}
-	n = 0;
-	while (s[j] != '\0')
-	{
-		s[n] = s[j];
-		j++;
+	i = n = 0;
+	if (s[0] != '\0')
+		i++;
+	if (accept[1] != '\0')
 		n++;
-	}
-	while (s[n] != '\0')
-	{
-		s[n] = '\0';
-		n++;
-	}
-	printf("the new sentence is %s\n", s);
+
+	return (s);
 }
