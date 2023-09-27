f71b203b2a678169b8159226a44067c1d088bf93
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-27 00:31:33 +0000

    A test

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,24 @@
 #include "main.h"
-
 /**
- * _ststr - i
+ * _strstr - i
  * @haystack: a
  * @needle: a
  *
  * Return: is a return
  */
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
+{
+	char *p, *n;
+
+	p = haystack;
+	n = needle;
+	if (p[1] == haystack[1])
+	{
+		p++;
+	}
+	if (n[1] == needle[1])
+	{
+		n++;
+	}
+	return (p);
+}
