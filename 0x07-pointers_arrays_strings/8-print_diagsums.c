a237e24392ff627ff348238cf5a3434a94ae30d4
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-09-27 00:37:02 +0000

    A test

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagsums - is not complete
+ * @a: i
+ * @size: s
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+
+	a[0] = 5;
+	if (a[i] < size)
+	{
+		size++;
+	}
+}
