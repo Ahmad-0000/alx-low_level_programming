5ccb5827b8f2fbe97369a76ee7088b35616ca47d
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-10-25 06:02:22 +0000

    Removed some sapces.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 /**
  * _pow_recursion - is a function to return the value of
- * 	 (x to the power y)
+ *	(x to the power y)
  * @x: is the base.
  * @y: is the power.
  *
  * Return: is to return either the result or (-1) if y < 0
- * 	or (1) if y is equal to 0;
+ *	or (1) if y is equal to 0;
  */
 
 int _pow_recursion(int x, int y)
