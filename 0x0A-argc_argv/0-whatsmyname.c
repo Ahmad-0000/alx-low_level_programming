7a2a32cf86346b3bd124781b0b948e4427c2ef2f
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-10-27 11:09:16 +0000

    Changed the return value of the program to 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 /**
  * main - is my main function that can be used to
  *	print the program name
- * @argc: is the number arguments that are passed to the program
- * @argv: is an array of pointers, each of which points
+ *@argc: is the number arguments that are passed to the program
+ *@argv: is an array of pointers, each of which points
  *	to an argument from the command line
  *
  * Return: is to return 1
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (1);
+		printf("%s\n", argv[0]);
+			return (0);
 }
