dac987858907e6889ea6ad899ecbe8e61f925f54
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-10-30 15:26:01 +0000

    Changed the function name

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * freegrid - is a function to free my
+ * free_grid - is a function to free my
  *	a space that is associated with
  *	my previous function
  * @g: is a pointer to the 2D grid
  * @h: is the height of the grid
  */
-void freegrid(int **g, int h)
+void free_grid(int **g, int h)
 {
 	int i;
 
