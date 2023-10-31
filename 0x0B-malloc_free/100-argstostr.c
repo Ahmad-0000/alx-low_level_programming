e3605536799ce8c5d87a8e0f3cdfd67be431ce65
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-10-31 03:08:09 +0000

    This is a function to concatenate the args from the cli, each one in a separate line

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * argstostr - is a function to concatenate the args
+ *	from the command line into 1 str, each one
+ *	with a '\n' after
+ * @ac: is the number of the args from the CLI
+ * @av: is a pointer to the array of the args
+ * Return: is to return a pointer to the new concatenated str.
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, c = 0;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+	p = (char *)malloc((c + ac) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	c = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, c++)
+		{
+			p[c] = av[i][j];
+			if (av[i][j + 1] == '\0')
+				p[++c] = '\n';
+		}
+	}
+	p[c] = '\0';
+	return (p);
+}
