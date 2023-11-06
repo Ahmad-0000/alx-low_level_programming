1ad741f50d2e64f5ff700f4d9411355109ac6a99
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-06 12:41:01 +0000

    Changed the type name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ struct dog
 /**
  * dog_t - I hate this project
  */
-typedef struct dog dot_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
