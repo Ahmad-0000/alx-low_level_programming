c9d67e3fb978c8340fc0942c0e2379fddc80ae1a
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-06 12:49:47 +0000

    The last solution

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * free_dog - is a function to free a memory that is ocuppied
+ *	by *d
+ * @d: is a pointer to the memory
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
