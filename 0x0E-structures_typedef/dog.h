cb11159d5749a9a47b23a89f947dc920a4798f86
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-06 10:59:26 +0000

    My first solution of the C structures project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - is a structure to hold information about dog
+ * @name: is the dog name
+ * @age: is the dog age
+ * @owner: is the owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
+#endif
