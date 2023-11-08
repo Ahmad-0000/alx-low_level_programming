f0b032b423c257654df7ae8dc1903b27fc01526e
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-08 14:38:57 +0000

    The solution of the 2nd task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - is a function to do implement a function
+ *	on every element of an array of integers
+ * @theArray: is a pointer to the array of integers
+ * @theSize: is the size of the array
+ * @action: is the function to be implemented
+ */
+void array_iterator(int *theArray, size_t theSize, void (*action)(int))
+{
+	int i;
+
+	if (theArray == NULL || theSize == 0 || action == NULL)
+		exit(1);
+	for (i = 0; i < theSize; i++)
+		action(theArray[i]);
+}
