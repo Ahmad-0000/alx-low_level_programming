aba2e2d06bd6817a74387fe5ae0b88091f1c9ae2
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-08 13:47:04 +0000

    A small change

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,10 @@
 /**
  * print_name - is a function to pass a pointer to a name
  *	to a function to handle it
- * @theName: is a pointer to the name
+ * @name: is a pointer to the name
  * @f: is a pointer to the function
  */
-void print_name(char *theName, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	f(theName);
+	f(name);
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define THIS_FILE
 #include <stdlib.h>
 int _putchar(char c);
-void print_name(char *theName, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *theArray, size_t theSize, void (*action)(int));
 int int_index(int *theArray, int theSize, int (*cmp)(int));
 #endif
