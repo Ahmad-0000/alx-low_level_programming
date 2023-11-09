fb625d9bd3561defc2fb099bb82dabf0fde0b3b8
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-09 15:03:30 +0000

    The solution of the 2nd task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - is a function to print a numbers
+ *	that are separated by a separator
+ * @separator: is the separator
+ * @n: is the number of the numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list p;
+
+	va_start(p, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			printf(i + 1 == n ? "%d\n" : "%d", va_arg(p, int));
+		else
+		{
+			printf(i + 1 == n ? "%d\n" : "%d", va_arg(p, int));
+			printf(i + 1 == n ? "" : "%s", separator);
+		}
+	}
+}
