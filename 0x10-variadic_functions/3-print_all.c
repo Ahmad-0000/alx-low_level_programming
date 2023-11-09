919345a53075bf798606c993e1acf915f1993717
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-09 19:55:36 +0000

    A small change

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,18 +1,22 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-struct options
-{
-	char c;
-	void (*ptr)(va_list);
-};
+/**
+ * ch - is to process characters
+ * @p: is to pull the argument
+ */
 void ch(va_list p)
 {
 	printf("%c", va_arg(p, int));
 }
+/**
+ * s - is to process strings
+ * @p: is to pull the argument
+ */
 void s(va_list p)
-{	
+{
 	char *a = va_arg(p, char *);
+
 	if (a == NULL)
 	{
 		printf("(nil)");
@@ -20,20 +24,34 @@ void s(va_list p)
 	}
 	printf("%s", a);
 }
+/**
+ * in - is to process integers
+ * @p: is to pull the argument
+ */
 void in(va_list p)
 {
 	printf("%d", va_arg(p, int));
 }
+/**
+ * f - is to process floats
+ * @p: is to pull the argument
+ */
 void f(va_list p)
 {
 	printf("%f", va_arg(p, double));
 }
+/**
+ * print_all - is a function to print any arguments
+ *	belong either to char or int or float or string.
+ * @theFormat: is a string that contains flags
+ */
 void print_all(const char * const theFormat, ...)
 {
 	int i = 0, j;
 	va_list p;
 	char c;
-	struct options a[] = {
+	char *theComma = "";
+	opt a[] = {
 		{'c', ch},
 		{'f', f},
 		{'i', in},
@@ -49,8 +67,9 @@ void print_all(const char * const theFormat, ...)
 		{
 			if (c == a[j].c)
 			{
+				printf("%s", theComma);
 				a[j].ptr(p);
-				printf(", ");
+				theComma = ", ";
 				break;
 			}
 			j++;
@@ -59,4 +78,3 @@ void print_all(const char * const theFormat, ...)
 	}
 	printf("\n");
 }
-
