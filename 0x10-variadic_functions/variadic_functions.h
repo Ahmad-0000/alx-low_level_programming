92f8bbbcad05b2b73e016c8b689190dff51cb727
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-10 01:15:40 +0000

    Removed the global variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef VAFUNCTIONS
 #define VAFUNCTIONS
+#include <stdarg.h>
+/**
+ * struct options - is to struct a structure that holds
+ *	a flag and a pointer to the associated function
+ * @c: is the flag
+ * @ptr: is a pointer to the associated function
+ */
+struct options
+{
+	char c;
+	void (*ptr)(va_list);
+};
+typedef struct options opt;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
