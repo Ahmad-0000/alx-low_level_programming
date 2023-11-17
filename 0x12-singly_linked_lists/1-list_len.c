a05b44ebb03e4351fec5f1b4d86be1714a3b7095
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-17 12:56:27 +0000

    The solution of the 2nd task

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * list_len - is a function to find the length of a list
+ * @h: is a pointer to the first node in the list
+ * Return: is to return the lenght of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
