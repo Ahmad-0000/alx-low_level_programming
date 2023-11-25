406096681594cb8bd76b274a41ba0b91d6683453
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-25 09:36:04 +0000

    If the index is not possible to be put in, no work will be done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n)
 		return (NULL);
 	for (i = 1; p->next != NULL && i < idx; i++)
 		p = p->next;
-	if (i >= idx)
+	if ((i > idx && (i - 1) == idx) || i == idx)
 	{
 		theNode->next = p->next;
 		theNode->n = n;
