ce7536daa121bc3e07d0c013a0ca8f8e65387dc3
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-26 16:20:24 +0000

    The solution of the 5th task

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int i);
 int set_bit(unsigned long int *n, unsigned int i);
 unsigned int powers(unsigned int theBase, unsigned int thePower);
+int clear_bit(unsigned long int *n, unsigned int i);
 #endif
