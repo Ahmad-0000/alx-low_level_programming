45c1a01ab7322f405b41460ab7d3814015fcfe27
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-11-26 16:17:07 +0000

    The solution of the 4th task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * set_bit - is a function
+ * @n: is a pointer
+ * @i: is an index
+ * Return: is to return 1 when success, or -1 when faliure
+ */
+
+int set_bit(unsigned long int *n, unsigned int i)
+{
+	unsigned int j;
+
+	if ((i + 1) > (sizeof(*n) * 8))
+		return (-1);
+	j = powers(2, i);
+	*n += j;
+	return (1);
+}
+
+/**
+ * powers - is a function
+ * @theBase: is the base
+ * @thePower: is the power
+ * Return: is to return the result
+ */
+
+unsigned int powers(unsigned int theBase, unsigned int thePower)
+{
+	unsigned int theResult = theBase, i;
+
+	if (thePower == 0)
+		return (1);
+	for (i = 1; i < thePower; i++)
+		theResult = theResult * theBase;
+	return (theResult);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,6 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int i);
+int set_bit(unsigned long int *n, unsigned int i);
+unsigned int powers(unsigned int theBase, unsigned int thePower);
 #endif
