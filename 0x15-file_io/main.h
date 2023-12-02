5f020a90d2c57319f52f8a966953ecea68134c72
Ahmad-0000 <ahmad.new.m.v@gmail.com>
2023-12-02 11:44:40 +0000

    Changed the added header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,6 @@
 #ifndef MIAN_H
 #define MAIN_H
-#include <stddef.h>
+#include <stdlib.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t l);
 int create_file(const char *filename, char *theContent);
