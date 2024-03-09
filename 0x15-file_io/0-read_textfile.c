066445394da46548b20139fe09f7dcf8269df422
Ahmad Husain Ali Basheer <ahmad@localhost.localdomain>
2024-03-09 07:26:18 +0200

    The solution of the 1st task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 
 /**
  * read_textfile - is a function to write the conten of a text file
- *	in the standard output
+ *  in to the POSIX standard output
  * @filename: is the name of the file to be written from
  * @l: is the number of the letters to be written
  * Return: is to return the number of the read and written chars,
@@ -13,27 +13,27 @@
  *	or the file can not be opened, the return value is 0
  */
 
-ssize_t read_textfile(const char *filename, size_t l)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *a;
-	int WrittenChars = 0;
-	int ReadChars = 0;
+	char *buffer;
+	int writtenChars = 0;
+	int readChars = 0;
 	int fd = 0;
 
 	if (filename == NULL)
 		return (0);
-	a = malloc(sizeof(*a) * l);
-	if (a == NULL)
+	buffer = malloc(sizeof(*buffer) * letters);
+	if (buffer == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	ReadChars = read(fd, a, l);
-	if (ReadChars == -1)
+	readChars = read(fd, buffer, letters);
+	if (readChars == -1)
 		return (0);
-	WrittenChars = write(STDIN_FILENO, a, ReadChars);
+	writtenChars = write(STDIN_FILENO, buffer, readChars);
 	close(fd);
-	if (WrittenChars == -1)
+	if (writtenChars == -1 || writtenChars != (int)letters)
 		return (0);
-	return (WrittenChars);
+	return (writtenChars);
 }
