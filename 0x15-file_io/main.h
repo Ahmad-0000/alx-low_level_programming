e72f0f9b5a8c832c98bf5499dffbbb1226aac66c
Ahmad Husain Ali Basheer <ahmad@localhost.localdomain>
2024-03-09 07:35:17 +0200

    A slight change

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,7 @@
 #ifndef MIAN_H
 #define MAIN_H
 #include <stdlib.h>
+#include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t l);
 int create_file(const char *filename, char *theContent);
