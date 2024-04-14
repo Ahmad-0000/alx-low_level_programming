b61c8d87aa6e84786912326f2d6146aaa5138e1c
Ahmad <ahmad.new.m.v@gmial.com>
2024-04-14 16:56:49 +0200

    Fixed the style, add documentation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 /**
  * create_file - is a function to create a file
  * @filename: is the filename
- * @theContent: is the content to be written inside the file
+ * @text_content: is the content to be written inside the file
  * Return: is to return 1 when success, or -1 when faliure
  */
 
@@ -16,23 +16,24 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, written_chars, length;
 
-    fd = open(filename, O_WRONLY | O_TRUNC);
-    umask(0);
-    if (errno == ENOENT)
-    {
-        fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-        if (fd == -1)
-            return (-1);
-    }
-    if (fd == -1)
-        return (-1);
-    if (text_content == NULL)
-        return (1);
-    length = 0;
-    while (text_content[length] != '\0')
-        length++;
-    written_chars = write(fd, text_content, length);
-    if (written_chars != length)
-        return (-1);
-    return (1);
+	fd = open(filename, O_WRONLY | O_TRUNC);
+	umask(0);
+	if (errno == ENOENT)
+	{
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+		if (fd == -1)
+			return (-1);
+	}
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	length = 0;
+	while (text_content[length] != '\0')
+		length++;
+	written_chars = write(fd, text_content, length);
+	if (written_chars != length)
+		return (-1);
+	return (1);
 }
+
