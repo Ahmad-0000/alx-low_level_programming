443e7655048f0fa1d542bc4e1e7b4170a5aa05ec
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-14 12:19:08 -0700

    Changed the style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,78 +16,78 @@
 
 int main(int argc, char *argv[])
 {
-    char buffer[1024];
-    int fd1, fd2;
-    int char_read = 1;
-    int char_written = 0;
-    int isclosed = 0;
+	char buffer[1024];
+	int fd1, fd2;
+	int char_read = 1;
+	int char_written = 0;
+	int isclosed = 0;
 
-    umask(0);
+	umask(0);
 	if (argc != 3)
-    {
-        fprintf(stderr, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-    fd1 = open(argv[1], O_RDONLY);
-    if (fd1 == -1)
-    {
-        fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
-    char_read = read(fd1, buffer, 1024);
-    if (char_read == -1)
-    {
-        fprintf(stderr, "Error: Cant't read from file %s\n", argv[1]);
-        close(fd1);
-        exit(98);
-    }
-    fd2 = open(argv[2], O_WRONLY | O_TRUNC);
-    if (fd2 == -1)
-    {
-        if (errno == ENOENT)
-        {
-            fd2 = open(argv[2], O_WRONLY | O_CREAT , S_IRUSR | S_IWUSR \
-                | S_IRGRP | S_IWGRP | S_IROTH);
-                if (fd2 == -1)
-                {
-                    fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-                    exit(99);
-                }
-        }
-        else
-        {
-            fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    }
-    while (char_read > 0)
-    {
-        char_written = write(fd2, buffer, char_read);
-        if (char_written == -1)
-        {
-            fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-            close(fd2);
-            exit(99);
-        }
-        char_read = read(fd1, buffer, 1024);
-        if (char_read == -1)
-        {
-              fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-              close(fd1);
-              exit(98);
-        }
-    }
-    isclosed = close(fd1);
-    if (isclosed == -1)
-    {
-        fprintf(stderr, "Can't close fd %d\n", fd1);
-        exit(100);
-    }
-    isclosed = close(fd2);
-    if (isclosed == -1)
-    {
-        fprintf(stderr, "Can't close fd %d\n", fd2);
-        exit(100);
-    }
-    return (0);
+	{
+		fprintf(stderr, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	char_read = read(fd1, buffer, 1024);
+	if (char_read == -1)
+	{
+		fprintf(stderr, "Error: Cant't read from file %s\n", argv[1]);
+		close(fd1);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC);
+	if (fd2 == -1)
+	{
+		if (errno == ENOENT)
+		{
+			fd2 = open(argv[2], O_WRONLY | O_CREAT , S_IRUSR | S_IWUSR \
+			| S_IRGRP | S_IWGRP | S_IROTH);
+			if (fd2 == -1)
+			{
+				fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+				exit(99);
+			}
+		}
+		else
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	while (char_read > 0)
+	{
+		char_written = write(fd2, buffer, char_read);
+		if (char_written == -1)
+		{
+			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			close(fd2);
+			exit(99);
+		}
+		char_read = read(fd1, buffer, 1024);
+		if (char_read == -1)
+		{
+			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+			close(fd1);
+			exit(98);
+		}
+	}
+	isclosed = close(fd1);
+	if (isclosed == -1)
+	{
+		fprintf(stderr, "Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	isclosed = close(fd2);
+	if (isclosed == -1)
+	{
+		fprintf(stderr, "Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
