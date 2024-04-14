7835b5d1b897424960f8269610b84b777f8192aa
Ahmad <ahmad.new.m.v@gmial.com>
2024-04-14 16:47:57 +0200

    Fixed 'double inculsion' proplem

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
-#ifndef MIAN_H
+#ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
 int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t l);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *theContent);
 int append_text_to_file(const char *filename, char *theContent);
 #endif
