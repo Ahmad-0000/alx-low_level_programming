e1298da7b77c24bc81fb45c15b5df02a074e6b15
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-14 07:28:15 -0700

    Changed in the style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,8 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	size_t writtenChars = 0;
 	size_t readChars = 0;
+	long int writtenCharsInt = 0;
+	long int readCharsInt = 0;
 	int fd = 0;
-
+	
 	if (filename == NULL)
 		return (0);
 	buffer = malloc(sizeof(*buffer) * letters);
@@ -29,12 +31,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	readChars = read(fd, buffer, letters);
-	if (readChars == -1)
+	readCharsInt = (int)readChars;
+	if (readCharsInt == -1)
 		return (0);
 	writtenChars = write(STDOUT_FILENO, buffer, readChars);
+	writtenCharsInt = (int)writtenChars;
 	close(fd);
-    free(buffer);
-	if (writtenChars == -1 || writtenChars != readChars)
-		return (0);
+	free(buffer);
+	if (writtenCharsInt == -1 || writtenChars != readChars)
+	return (0);
 	return (writtenChars);
 }
