158e04cb0f0ba0cbb839c03f344a33aa6142c0f6
Ahmad <ahmad.new.m.v@gmial.com>
2024-04-14 15:51:50 +0200

    handled memory leaks, and another situation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,17 +7,17 @@
  * read_textfile - is a function to write the conten of a text file
  *  in to the POSIX standard output
  * @filename: is the name of the file to be written from
- * @l: is the number of the letters to be written
- * Return: is to return the number of the read and written chars,
- *	or if filename is null, or read has failed, or write has failed
+ * @letters: is the number of the letters to be written
+ * Return: is to return the number of the read and written chars.
+ *	If "filename" is null, or "read" failed, or "write" failed
  *	or the file can not be opened, the return value is 0
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	int writtenChars = 0;
-	int readChars = 0;
+	size_t writtenChars = 0;
+	size_t readChars = 0;
 	int fd = 0;
 
 	if (filename == NULL)
@@ -33,7 +33,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	writtenChars = write(STDOUT_FILENO, buffer, readChars);
 	close(fd);
-	if (writtenChars == -1 || writtenChars != (int)letters)
+    free(buffer);
+	if (writtenChars == -1 || writtenChars != readChars)
 		return (0);
 	return (writtenChars);
 }
