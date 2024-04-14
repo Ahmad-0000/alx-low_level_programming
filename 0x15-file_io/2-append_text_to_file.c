6bc12a97e60b7d59fab4c1f98fa9f726b377b3f3
Ahmad <ahmad.new.m.v@gmail.com>
2024-04-14 08:17:45 -0700

    Fixed the style and changed the documentation

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,29 +5,29 @@
 /**
  * append_text_to_file - is a function to append a text to a file
  * @filename: is the name of the file to be appended to
- * @theContent: is the text to be appended
+ * @text_content: is the text to be appended
  * Return: is to return 1 when success, or -1 when faliure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = 0;
-    int length;
-    int written_chars;
+	int length;
+	int written_chars;
 
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
-    if (text_content == NULL)
-        return (1);
-    length = 0;
-    while (text_content[length] != '\0')
-        length++;
-    written_chars = write(fd, text_content, length);
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	length = 0;
+	while (text_content[length] != '\0')
+		length++;
+	written_chars = write(fd, text_content, length);
 
-    if (written_chars != length)
-        return (-1);
-    return (1);
+	if (written_chars != length)
+		return (-1);
+	return (1);
 }
