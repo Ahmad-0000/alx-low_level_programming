951d2e1704c9baa87fd787c328b7789bd1cd9c26
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-17 22:13:40 +0000

    No initial loop decleration in c89 standard

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,25 +9,25 @@
 
 void print_times_table(int n)
 {
-	int result;
+	int result, i, j;
 
 	if (n < 0 || n > 15)
 		return;
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
 			if (result < 10)
 			{
 				if (j != 0)
-					print_space(3);
+					printing_space(3);
 				_putchar(result % 10 + '0');
 				_putchar(j == n ? '\n' : ',');
 			}
 			else if (result < 100)
 			{
-				print_space(2);
+				printing_space(2);
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
 				_putchar(j == n ? '\n' : ',');
