57f57b36e104ef7e71d29408689c27a95d08d24a
Ahmad <ahmad.new.m.v@gamil.com>
2024-04-17 20:29:34 +0000

    Changed the solution

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,31 +2,14 @@
 #include <limits.h>
 /**
  * print_last_digit - is my function to print the last digit of a number
- * @i: is the number to print last digit for
- * Return: is to return a value
+ * @i: is the number to print last digit of
+ * Return: is to return the value of the last digit
  */
 
 int print_last_digit(int i)
 {
 	if (i < 0)
-	{
 		i = -i;
-		_putchar(i % 10 + '0');
-	}
-	else if (i == 0)
-	{
-		_putchar('0');
-	}
-	else if (i == INT_MIN)
-	{
-		_putchar(8 + '0');
-	}
-	else
-	{
-		_putchar(i % 10 + '0');
-	}
-	if (i == INT_MIN)
-		return (8);
-	else
-		return (i % 10);
+	_putchar(i % 10 + '0');
+	return i % 10;
 }
