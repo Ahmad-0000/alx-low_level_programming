06593a3aa4dda02cfa6e1a1b43121d9b38928109
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-11 08:12:52 +0000

    Removed a bug

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
+#define FOUND 1
+#define NOT_FOUND 0
 
 /**
  * hash_table_set - is a function to add a key-value pair to @ht
@@ -12,11 +14,10 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int length;
-	int i;
+	int length, i, key_comparison = 1, found = NOT_FOUND;
 	unsigned long index;
 	char *duplicated_value;
-	hash_node_t *node;
+	hash_node_t *node, *temp;
 
 	if (!ht || !key)
 		return (0);
@@ -33,18 +34,43 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	for (i = 0; i < length; i++)
 		duplicated_value[i] = value[i];
 	node->value = duplicated_value;
-	if (!(ht->array[index]))
-	{
-		ht->array[index] = node;
-	}
-	else if (ht->array[index] && ht->array[index]->key == key)
+	if (ht->array[index])
+		key_comparison = strcmp(ht->array[index]->key, key);
+	if (!(ht->array[index]) || (ht->array[index] && key_comparison))
 	{
 		ht->array[index] = node;
 	}
 	else
 	{
-		node->next = ht->array[index]->next;
-		ht->array[index]->next = node;
+		temp = ht->array[index];
+		found = collision_update(temp, node);
+		if (!found)
+		{
+			node->next = ht->array[index]->next;
+			ht->array[index]->next = node;
+		}
 	}
 	return (1);
+}
+
+/**
+ * collision_update - is a function to update a collided node
+ * @head: is the first node in the list
+ * @node: is the node new node
+ * Return: is to return 1 when success, otherwise, 0
+ */
+
+int collision_update(hash_node_t *head, hash_node_t *node)
+{
+	while (head->next != NULL)
+	{
+		if (!strcmp(head->next->key, node->key))
+		{
+			node = head->next->next;
+			head->next = node;
+			return (FOUND);
+		}
+		head = head->next;
+	}
+	return (NOT_FOUND);
 }
