1b209ffdc35627241d8b6643850fbbe54400494a
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-11 13:06:22 +0000

    Updated the header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,4 +34,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int collision_update(hash_node_t *temp, hash_node_t *node);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 #endif
