e49a575ed157629f090550c1ab184f5df3f44b3b
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-12 08:34:53 +0000

    Improved readability

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,28 +1,30 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - is a function
- * @h: is a pointer
+ * add_nodeint_end - is a function to add a node in the end of a list
+ * @head: is a pointer to a pointer to the first node in the list
  * @n: is an integer
- * Return: is to return a value
+ * Return: is to return a pointer to the last node
  */
 
-listint_t *add_nodeint_end(listint_t **h, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *theLast = malloc(sizeof(listint_t));
-	listint_t *tmp = *h;
+	listint_t *last_node;
+	listint_t *temp;
 
-	if (theLast == NULL)
+	last_node = malloc(sizeof(*last_node));
+	if (last_node == NULL)
 		return (NULL);
-	theLast->n = n;
-	theLast->next = NULL;
-	if (*h == NULL)
+	last_node->n = n;
+	last_node->next = NULL;
+	temp = *head;
+	if (temp == NULL)
 	{
-		*h = theLast;
-		return (theLast);
+		*head = last_node;
+		return (last_node);
 	}
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = theLast;
-	return (theLast);
+	while (temp->next)
+		temp = temp->next;
+	temp->next = last_node;
+	return (last_node);
 }
