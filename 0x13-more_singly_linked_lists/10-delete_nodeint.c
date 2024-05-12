ba3583e4edbc5ee76da3b03c8b78eab108dc7cc8
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-12 08:57:38 +0000

    Improved readability

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,33 +1,34 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - is a function to delete a node the index i
- *	(starting from 0)
- * @h: is a pointer to a pointer to the node
- * @i: is the index
- * Return: is to return 1 when success, or 0 when faliure
+ * delete_nodeint_at_index - is a function to delete a node at the index @index
+ * @head: is a pointer to a pointer to the first node in the list
+ * @index: is the index
+ * Return: is to return 1 when success, or -1 when faliure
  */
-int delete_nodeint_at_index(listint_t **h, unsigned int i)
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int j;
-	listint_t *p, *tmp;
+	listint_t *traveller, *temp;
 
-	if (h == NULL || *h == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
-	p = *h;
-	tmp = p;
-	if (i == 0)
+	traveller = *head;
+	temp = *head;
+	if (index == 0)
 	{
-		*h = (*h)->next;
-		free(tmp);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
-	for (j = 1; p != NULL && j < i; j++)
-		p = p->next;
-	if (j > i - 1)
+	for (j = 1; traveller != NULL && j < index; j++)
+		traveller = traveller->next;
+	if (j > index - 1)
 	{
-		tmp = p->next;
-		p->next = p->next->next;
-		free(tmp);
+		temp = traveller->next;
+		traveller->next = traveller->next->next;
+		free(temp);
 		return (1);
 	}
 	return (-1);
