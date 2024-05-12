3c267be8a50fff5e49c3687874f80ef0ee79c2ba
Ahmad <ahmad.new.m.v@gamil.com>
2024-05-12 08:42:23 +0000

    Conformed to the documentation style

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,7 @@
  * pop_listint - is a function to delete the last element of the list
  * @head: is a pointer to a pointer to the first node in the list
  * Return: is to return the number that was stored in the popped node
- * 	or 0 if no such node
+ *	or 0 if no such node
  */
 
 int pop_listint(listint_t **head)
