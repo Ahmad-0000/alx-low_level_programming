da1252394fa0e88c09237adf689993b3d7b2354c
Ahmad <ahmad.new.m.v@gmial.com>
2024-05-25 20:11:32 +0200

    Conformed to the style (no more than 80 character lines)

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,5 @@ int update_collision(hash_node_t *temp, hash_node_t *node);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-void insert_node(hash_table_t *ht, hash_node_t *node, unsigned long idx, int cmp);
+void insert_node(hash_table_t *ht, hash_node_t *node, unsigned long i, int c);
 #endif
