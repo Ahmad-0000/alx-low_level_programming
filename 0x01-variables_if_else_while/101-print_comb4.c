65e5e5df11d045e0375c3c5875564c81362b0d92
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-06 12:28:54 +0000

    Added the solution of the 2nd advanced task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - printing all possible different combinations of three digits,
+ * 	  only the smallest combination
+ * Return: to return 0
+*/
+
+int main(void)
+{
+	int a, b, c;
+
+	a = 0;
+	while (a < 9)
+	{
+		b = a + 1;
+		while (b <= 9)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+				if (a != 7 || b != 8 || c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
