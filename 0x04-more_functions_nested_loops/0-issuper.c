e439796a5544c9acb1dfee79584a95964ae2ed54
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-07 13:44:12 +0000

    Readable documentation

--- 0x04-more_functions_nested_loops/0-issuper.c
+++ 0x04-more_functions_nested_loops/0-issuper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * _isupper - is a function to determine whether a letter is uppercase or no
+ * _isupper - is a function to determine @c is an uppercase letter or not
  * @c: is the letter to check
- * Return: is to return 1 if uppercase or 0 otherwise
+ * Return: is to return 1 if it's uppercase or 0 otherwise
  */
+
 int _isupper(int c)
 {
 	if (c < 65 || c > 90)
