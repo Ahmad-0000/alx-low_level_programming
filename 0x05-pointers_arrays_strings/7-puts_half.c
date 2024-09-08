7fcc4012e1cbe53769625b639e47be451b965467
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-08 15:13:04 +0000

    Better solution

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,33 @@
 #include "main.h"
+
 /**
  * puts_half - is a function to print the last half of a string
  * @str: is a pointer to the string
  */
+
 void puts_half(char *str)
 {
-	int i, n;
+	int i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
+	while (str[i])
 		i++;
-	}
 	if (i % 2 == 0)
 	{
-		n = (i / 2);
-		while (str[n] != '\0')
+		i /= 2;
+		while (str[i])
 		{
-			_putchar(str[n]);
-			n++;
+			_putchar(str[i]);
+			i++;
 		}
 	}
 	else
 	{
-		n = i / 2 + 1;
-		while (str[n] != '\0')
+		i = i / 2 + 1;
+		while (str[i])
 		{
-			_putchar(str[n]);
-			n++;
+			_putchar(str[i]);
+			i++;
 		}
 	}
 	_putchar('\n');
