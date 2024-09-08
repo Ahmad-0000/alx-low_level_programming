d85ef7a9033020368ae519bb63eb40d39c80342a
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-08 05:15:49 +0000

    Removed a bug

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - is a function to print @n
  * @n: is a number to print
@@ -6,31 +7,34 @@
 
 void print_number(int n)
 {
-	int i;
-
+	int i, tmp = n, special = 0;
+	
+	if (n == -2147483648)
+	{
+		special = 1;
+		n++;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n = tmp = -n;
 	}
 	else if (!n)
 	{
 		_putchar('0');
 		return;
 	}
-	for (i = 1; i >= 1; i *= 10)
-	{
-		if (i > n)
-		{
-			i /= 10;
-			break;
-		}
-	}
+	for (i = 1; n > 9; i *= 10)
+		n /= 10;
+	n = tmp;
 	while (1)
 	{
 		if (i == 1)
 		{
-			_putchar(n + '0');
+			if (special && i == 1)
+				_putchar(n + 49);
+			else
+				_putchar(n + 48);
 			return;
 		}
 		_putchar(n / i + '0');
