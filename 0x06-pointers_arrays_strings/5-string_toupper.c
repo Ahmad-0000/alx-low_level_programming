665a0c78649c1ea5c7919e71790e01525804b5a0
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-09 15:40:29 +0000

    A better solution

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * string_toupper - is a function to convert a string to uppercase
  * @c: is a pointer to the string
  * Return: is to return a pointer to the string after conversion
  */
+
 char *string_toupper(char *c)
 {
 	int i = 0;
@@ -11,14 +13,8 @@ char *string_toupper(char *c)
 	while (c[i] != '\0')
 	{
 		if (c[i] >= 97 && c[i] <= 122)
-		{
 			c[i] -= 32;
-			i++;
-		}
-		else
-		{
-			i++;
-		}
+		i++;
 	}
 	return (c);
 }
