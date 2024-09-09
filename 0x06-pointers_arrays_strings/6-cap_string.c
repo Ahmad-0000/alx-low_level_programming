ee7f5251dd68db75419ea1cc3f26e1b78674b206
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-09 19:00:51 +0000

    The solution of 6th task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,26 +1,45 @@
 #include "main.h"
+#define FIRST_LETTER 1
+#define ELSE 0
+
 /**
  * cap_string - is to captialize the first letter from every word in a string
  * @c: is a pointer to the string
  * Return: is to return the product string.
  */
+
 char *cap_string(char *c)
 {
-	int i = 0;
+	int i, j;
+	int where = ELSE;
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.', '!',
+	       	               '?', '"', '(', ')', '{','}'};
 
+	i = 0;
 	while (c[i] != '\0')
 	{
-		if ((i != 0 && c[i] >= 97) && (c[i] <= 122) &&
-		(c[i - 1] < 65 || c[i - 1] > 90) &&
-		(c[i - 1] < 97 || c[i - 1] > 122))
+		for (j = 0; j < 13; j++)
+		{
+			if (c[i] == separators[j])
+			{
+				where = FIRST_LETTER;
+				goto next_letter;
+			}
+		}
+		if (c[i] >= 97 && c[i] <= 122 && where)
 		{
 			c[i] -= 32;
-			i++;
+			where = ELSE;
 		}
 		else
 		{
-			i++;
+			for (j = 0; j < 13; j++)
+				if (c[i] == separators[j])
+					goto next_letter;
+			where = ELSE;
 		}
+		next_letter:
+		i++;
 	}
 	return (c);
 }
