ccdcc6cc764ecb211ef705a9bd476029f8dcd84d
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-10 06:18:49 +0000

    Last mandatory task solutio

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,44 +1,26 @@
 #include "main.h"
+
 /**
- * leet - is a function to make a code from a letter
- * @c: is a pointer to the letter to make the code from
+ * leet - is a function to encode @c into 1337
+ * @c: is a string
  * Return: is to return a pointer to the coded letter
  */
+
 char *leet(char *c)
 {
-	int i = 0;
+	int i, j;
+	char letters[5] = {'A', 'E', 'O', 'T', 'L'};
+	char digits[5] = {'4', '3', '0', '7', '1'};
 
+	i = 0;
 	while (c[i] != '\0')
 	{
-		if (c[i] == 65 || c[i] == 90)
+		for (j = 0; j < 5; j++)
 		{
-			c[i] = 4 + '0';
-			i++;
+			if (c[i] == letters[j] || c[i] == (letters[j] + 32))
+				c[i] = digits[j];
 		}
-		else if (c[i] == 69 || c[i] == 94)
-		{
-			c[i] = 3 + '0';
-			i++;
-		}
-		else if (c[i] == 111 || c[i] == 79)
-		{
-			c[i] = 0 + '0';
-			i++;
-		}
-		else if (c[i] == 84 || c[i] == 116)
-		{
-			c[i] = 7 + '0';
-			i++;
-		}
-		else if (c[i] == 76 || c[i] == 108)
-		{
-			c[i] = 1 + '0';
-			i++;
-		}
-		else
-		{
-			i++;
-		}
+		i++;
 	}
 	return (c);
 
