e92f046b34f0a4bde0aec8a630c4c77e952f0621
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-11 15:21:05 +0000

    A real solution

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,20 +1,25 @@
 #include "main.h"
+
 /**
- * _strpbrk - i
- * @s: a
- * @accept: is an accept
- *
- * Return: is a return
+ * _strpbrk - is a function analog to the standard "strpbrk" function
+ * @s: is a pointer to the string to search in
+ * @accept: is a pointer of the string containing the accepted characters
+ * Return: is to return a pointer to the first occurence to any of the
+ * 	characters in "accept" in "s"
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int i, j;
 
-	i = n = 0;
-	if (s[0] != '\0')
+	i = 0;
+	while (s[i])
+	{
+		for (j = 0; accept[j]; j++)
+			if (s[i] == accept[j])
+				return (&(s[i]));
 		i++;
-	if (accept[1] != '\0')
-		n++;
+	}
 
-	return (s);
+	return (NULL);
 }
