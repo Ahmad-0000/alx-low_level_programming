1572d7d4cfd1e8c1a5abc8d0b9049ba0d189b75c
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-11 21:06:45 +0000

    7th mandatory task solution

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - as the name
+ * @a: a pointer to an array of 8 characters - 8 arrays = 64 square
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			putchar(a[i][j]);
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
