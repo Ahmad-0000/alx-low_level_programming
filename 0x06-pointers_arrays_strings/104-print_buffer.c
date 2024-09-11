764d77c468cbdce861783276b7bc2c3c63803761
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-11 12:01:13 +0000

    Removed a bug

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,31 +3,31 @@
 
 /**
  * print_buffer - is a function to a buffer in the following format:
- * 	* it prints the content of size bytes of @b
- * 	* 10 bytes per line
- * 	* Line starts with the position its first byte in hexadecimal
- * 	* Line has the hexadecimal content (2 chars) of @b, 2 bytes at a time
- * 	* Line has the content of @b: printable => as it is, otherwise .
- * 	* Each line ends with a new line \n
- * 	* If size is 0 or less, the output should be a new line only \n
+ *	* it prints the content of sz bytes of @b
+ *	* 10 bytes per line
+ *	* Line starts with the position its first byte in hexadecimal
+ *	* Line has the hexadecimal content (2 chars) of @b, 2 bytes at a time
+ *	* Line has the content of @b: printable => as it is, otherwise .
+ *	* Each line ends with a new line \n
+ *	* If sz is 0 or less, the output should be a new line only \n
  * @b: a pointer to the buffer
- * @size: the size of the buffer to be printed
+ * @sz: the size of the buffer to be printed
  */
 
-void print_buffer(char *b, int size)
+void print_buffer(char *b, int sz)
 {
 	int i, j, k, ll, sp, unprintable[33];
 
 	for (i = 0; i < 32; i++)
 		unprintable[i] = i;
 	unprintable[i] = 127;
-	if (size <= 0)
+	if (sz <= 0)
 		putchar('\n');
-	for (i = 0; i < size; i += 10)
+	for (i = 0; i < sz; i += 10)
 	{
 		ll = 0;
 		printf("%08x: ", i);
-		for (j = i, sp = 1; (j < 10 + i) && j < size; j++, sp++, ll++)
+		for (j = i, sp = 1; (j < 10 + i) && j < sz; j++, sp++, ll += 2)
 		{
 			if (!(sp % 3))
 			{
@@ -40,7 +40,7 @@ void print_buffer(char *b, int size)
 		putchar(' ');
 		for (; ll < LINE_LENGTH; ll++)
 			putchar(' ');
-		for (j = i; j < (10 + i) && j < size; j++)
+		for (j = i; j < (10 + i) && j < sz; j++)
 		{
 			for (k = 0; k < 33; k++)
 				if (b[j] == unprintable[k])
