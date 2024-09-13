9c70f19cb9e7496e52c55a2f5e84b64c929ada10
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-13 14:39:14 +0000

    Last advanced task solution

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 #include <stdio.h>
+
 int WordsCounting(char *str)
 {
 	int i, theWords = 0, inWord = 0;
@@ -19,105 +20,104 @@ int WordsCounting(char *str)
 	}
 	return (theWords);
 }
-/**
- * Filling - is a function to fill the 2D array that was
- *	allocated dynamically using malloc with the correct
- *	order of the characters
- * @p: is a double pointer to the 2D array
- * @str: is the string to be cut
- * @maxLen: is the length of the longest word in the array
- *	(the trailing 1 is due to the null terminator)
- * Return: is to return a double pointer to the filled 2D array
- */
-char **Filling(char **p, char *str)
+
+
+int *wlengths(char *str, int words)
 {
-	int i, j, c = 0, inWord = 0;
+	int *lengths;
+	int i, j;
 
-
-	for (i = 0; p[i] != NULL ; i++)
+	lengths = malloc(sizeof(int) * words);
+	if (!lengths)
+		return (NULL);
+	for (i = 0; i < words; i++)
+		lengths[i] = 0;
+	for (i = 0; str[i] == ' '; i++)
+		;
+	j = 0;
+	for (; str[i]; i++)
 	{
-		for (j = 0; p[i][j] != '\0'  ; c++)
+		if (str[i] != ' ')
 		{
-			if (str[c] != ' ' && str[c] != '\t' && inWord == 0)
-			{
-				p[i][j] = str[c];
-				inWord = 1;
-				j++;
-			}
-			else if (inWord == 1 && str[c] != '\t' && str[c] != ' ')
-			{
-				p[i][j] = str[c];
-				j++;
-			}
-			else if ((str[c] == ' ' || str[c] == '\t') && inWord == 1)
-			{
-				p[i][j] = '\0';
-				inWord  = 0;
-				c++;
-				break;
-			}
-			if (p[i][j] == '\0')
-				c++;
+			lengths[j]++;
+		}
+		else
+		{
+			for (; str[i] == ' '; i++)
+				;
+			i--;
+			j++;
 		}
 	}
-	return (p);
+	return (lengths);
 }
 
-/**
- * strtow - is a function to split a 2D array into words
- *	(basically by filling a 2D array with them),
- *	each of which in a separate array and is null terminated
- * @str: is the string to work upon
- * Return: is to return a pointer to the filled 2D array
- */
+void freeall(char **array, int size)
+{
+	int i = 0;
+
+	while (i < size)
+		free(array[i]);
+	free(array);
+}
+
+char *ext(char *ps, int length, int *ps2)
+{
+	int i = 0;
+	char *word;
+
+	word = malloc(sizeof(*ps) * (length + 1));
+	if (!word)
+		return (NULL);
+	while (i < length)
+	{
+		word[i] = *ps;
+		ps++;
+		i++;
+		(*ps2)++;
+	}
+	return (word);
+}
+
 char **strtow(char *str)
 {
-	char **p;
-	int i, inWord = 0, theWords = 0, j = 0, theLen = 0, f = 1;
+	int words, i, *lengths, j = 0;
+	char *a, **warray;
 
-	if (str == NULL)
+	if (!str || !(*str))
 		return (NULL);
-	theWords = WordsCounting(str);
-	printf("The words are not really %d\n", ++theWords);
-	p = ((theWords - 1) == 0) ? NULL : (char **)malloc(sizeof(char *) * theWords);
-	if (p == NULL)
-		return (NULL);
-	p[theWords - 1] = NULL;
-	for (i = 0; str[i] != '\0'; i++)
+	words = WordsCounting(str);
+	a = malloc(sizeof(char *) * (words + 1));
+	lengths = wlengths(str, words);
+	warray = malloc(sizeof(char *) * (words + 1));
+	if (!a || !lengths || !warray)
 	{
-		if ((str[i] == ' ' || str[i] == '\t') && f == 1)
-			continue;
-		if (str[i] != ' ' && str[i] != '\t' && inWord == 0)
-		{
-			inWord = 1;
-			f = 0;
-		}
-		else if (str[i] == ' ' || str[i] == '\t')
-		{
-			inWord = 0;
-			printf("The length of the word is %d\n", theLen);
-			if (theLen != 0)
-				p[j++] = (char *)malloc(sizeof(char) * (theLen + 1));
-			if (p[j - 1] == NULL && j != theWords)
-				return (NULL);
-			p[j - 1][theLen] = (theLen != 0) ? '\0' : p[j - 1][theLen];
-			for (int k = 0; k < theLen; k++)
-				p[j - 1][k] = 1;
-			theLen = 0;
-		}
-		if (inWord == 1)
-			theLen++;
+		free(a);
+		free(lengths);
+		free(warray);
+		return (NULL);
 	}
-	if (theLen != 0)
+	for (i = 0; i < words; i++)
 	{
-		p[j - 1] = (char *)malloc(sizeof(char) * (theLen + 1));
-		printf("The last lenght is %dand j is %d\n", theLen, j);
-		if (p[j - 1] == NULL)
+		warray[i] = malloc(sizeof(char *));
+		if (!warray[i])
+		{
+			freeall(warray, i + 1);
 			return (NULL);
-		p[j][theLen] = '\0';
-		for (int k = 0; k < theLen; k++)
-			p[j - 1][k] = 1;
-
-	}	
-	return (Filling(p, str));
+		}
+	}
+	warray[i] = NULL;
+	for (i = 0; str[i] == ' '; i++)
+		;
+	while (str[i])
+		if (str[i] != ' ')
+		{
+			warray[j] = ext(&(str[i]), lengths[j], &i);
+			j++;
+		}
+		else
+			i++;
+	free(a);
+	free(lengths);
+	return (warray);
 }
