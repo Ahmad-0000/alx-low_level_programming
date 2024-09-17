88cea007189abf3eadd0404b24351379de64eb13
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-17 06:16:34 +0000

    Added a new prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -43,4 +43,5 @@ int checkloop(tracklist_t *head, const listint_t *ptr);
 void freetracker(tracklist_t *head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 #endif
