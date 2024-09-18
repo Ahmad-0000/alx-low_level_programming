496605da6d354b9ab2651c14e73793df4d263d94
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-18 13:18:25 +0000

    1st advanced task solution

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,22 @@
 #ifndef MIAN_H
 #define MAIN_H
 #include <stdlib.h>
+
+/**
+ * union temp: a union used to check the endianness of a CPU
+ * @data: to hold a number spans two bytes
+ * @s: a structure with 2 members each spans 1 byte
+ */
+
+union temp
+{	unsigned data;
+	struct temp2
+	{
+		unsigned b :8;
+		unsigned c :8;
+	} s;
+} u;
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -10,4 +26,5 @@ unsigned int powers(unsigned int theBase, unsigned int thePower);
 int clear_bit(unsigned long int *n, unsigned int i);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int isNotZero(unsigned long int n, unsigned int i);
+int get_endianness(void);
 #endif
