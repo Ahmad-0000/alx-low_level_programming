78149850c5df73b20228e6d61c0f623b304df41f
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-21 06:55:25 +0000

    Removed some bugs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 #include "main.h"
 
 /**
- * main - the main function
+ * main - a program like standard "cp" program in linux
  * @argc: is the number of command line arguments
  * @argv: the array of pointers to the command line arguments
  * Return: is to return 1 when success
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
 {
 	char buffer[1024];
 	int fd1, fd2;
-	int char_read = 1;
-	int char_written = 0;
-	int isclosed = 0;
+	int char_read;
+	int char_written;
+	int isclosed;
 
 	umask(0);
 	if (argc != 3)
@@ -31,13 +31,13 @@ int main(int argc, char *argv[])
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stderr, "Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	char_read = read(fd1, buffer, 1024);
 	if (char_read == -1)
 	{
-		fprintf(stderr, "Error: Cant't read from file %s\n", argv[1]);
+		fprintf(stderr, "Cant't read from file %s\n", argv[1]);
 		close(fd1);
 		exit(98);
 	}
@@ -50,13 +50,15 @@ int main(int argc, char *argv[])
 			| S_IRGRP | S_IWGRP | S_IROTH);
 			if (fd2 == -1)
 			{
-				fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+				fprintf(stderr, "Can't write to %s\n", argv[2]);
+				close(fd1);
 				exit(99);
 			}
 		}
 		else
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			fprintf(stderr, "Can't write to %s\n", argv[2]);
+			close(fd1);
 			exit(99);
 		}
 	}
@@ -65,26 +67,26 @@ int main(int argc, char *argv[])
 		char_written = write(fd2, buffer, char_read);
 		if (char_written == -1)
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			fprintf(stderr, "Can't write to %s\n", argv[2]);
+			close(fd1);
 			close(fd2);
 			exit(99);
 		}
 		char_read = read(fd1, buffer, 1024);
 		if (char_read == -1)
 		{
-			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+			fprintf(stderr, "Can't read from file %s\n", argv[1]);
 			close(fd1);
+			close(fd2);
 			exit(98);
 		}
 	}
-	isclosed = close(fd1);
-	if (isclosed == -1)
+	if (close(fd1) == -1)
 	{
 		fprintf(stderr, "Can't close fd %d\n", fd1);
 		exit(100);
 	}
-	isclosed = close(fd2);
-	if (isclosed == -1)
+	if (close(fd2) == -1)
 	{
 		fprintf(stderr, "Can't close fd %d\n", fd2);
 		exit(100);
