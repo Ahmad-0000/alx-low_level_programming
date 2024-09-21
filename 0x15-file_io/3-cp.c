faa5fa75d7ab3eb80dd98f5fef2c039a187d2aca
Ahmad <ahmad.new.m.v@gmail.com>
2024-09-21 07:13:12 +0000

    A better solution

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,19 +25,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-	{
-		fprintf(stderr, "Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	char_read = read(fd1, buffer, 1024);
-	if (char_read == -1)
-	{
-		fprintf(stderr, "Cant't read from file %s\n", argv[1]);
-		close(fd1);
-		exit(98);
-	}
 	fd2 = open(argv[2], O_WRONLY | O_TRUNC);
 	if (fd2 == -1)
 	{
@@ -47,45 +34,86 @@ int main(int argc, char *argv[])
 			| S_IRGRP | S_IWGRP | S_IROTH);
 			if (fd2 == -1)
 			{
-				fprintf(stderr, "Can't write to %s\n", argv[2]);
-				close(fd1);
+				fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
 				exit(99);
 			}
 		}
 		else
 		{
-			fprintf(stderr, "Can't write to %s\n", argv[2]);
-			close(fd1);
+			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		if (close(fd2) == -1)
+		{
+			fprintf(stderr, "Error: Can't close fd %d\n", fd2);
+			exit(100);
+		}
+		exit(98);
+	}
+	char_read = read(fd1, buffer, 1024);
+	if (char_read == -1)
+	{
+		fprintf(stderr, "Error: Cant't read from file %s\n", argv[1]);
+		if (close(fd1) == -1)
+		{
+			fprintf(stderr, "Error: Can't close fd %d\n", fd1);
+			exit(100);
+		}
+		if (close(fd2) == -1)
+		{
+			fprintf(stderr, "Error: Can't close fd %d\n", fd2);
+			exit(100);
+		}
+		exit(98);
 	}
 	while (char_read > 0)
 	{
 		char_written = write(fd2, buffer, char_read);
 		if (char_written == -1)
 		{
-			fprintf(stderr, "Can't write to %s\n", argv[2]);
-			close(fd1);
-			close(fd2);
+			fprintf(stderr, "Error: `Can't write to %s\n", argv[2]);
+			if (close(fd1) == -1)
+			{
+				fprintf(stderr, "Error: Can't close fd %d\n", fd1);
+				exit(100);
+			}
+			if (close(fd2) == -1)
+			{
+				fprintf(stderr, "Error: Can't close fd %d\n", fd2);
+				exit(100);
+			}
 			exit(99);
 		}
 		char_read = read(fd1, buffer, 1024);
 		if (char_read == -1)
 		{
-			fprintf(stderr, "Can't read from file %s\n", argv[1]);
-			close(fd1);
-			close(fd2);
+			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+			if (close(fd1) == -1)
+			{
+				fprintf(stderr, "Error: Can't close fd %d\n", fd1);
+				exit(100);
+			}
+			if (close(fd2) == -1)
+			{
+				fprintf(stderr, "Error: Can't close fd %d\n", fd2);
+				exit(100);
+			}
 			exit(98);
 		}
 	}
 	if (close(fd1) == -1)
 	{
-		fprintf(stderr, "Can't close fd %d\n", fd1);
+		fprintf(stderr, "Error: Can't close fd %d\n", fd1);
 		exit(100);
 	}
 	if (close(fd2) == -1)
 	{
-		fprintf(stderr, "Can't close fd %d\n", fd2);
+		fprintf(stderr, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);
