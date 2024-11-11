c1ce09f64975c4d1fc338338c1bee88fa546aef1
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-11 15:10:12 +0000

    Removed a bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,7 +30,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new_node->next = *h;
 		if (*h)
-			(*h)->next = new_node;
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
 	if (i != idx)
