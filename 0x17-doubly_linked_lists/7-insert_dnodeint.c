547d79dbeb3b2bb7f3801af13dcb793a40647a46
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-11 15:02:51 +0000

    No bugs

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/**
+/******
  * insert_dnodeint_at_index - is a function to insert a node in a doubly linked
  *	list at the idx-th position (idx starts at 0).
  * @h: is a pointer to a pointer to the first node in the list.
@@ -15,25 +15,42 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	new_node = malloc(sizeof(*new_node));
-	if (h == NULL || *h == NULL || new_node == NULL)
-		return (NULL);
-	temp = *h;
-	for (i = 1; i < idx && temp->next != NULL; i++)
-		temp = temp->next;
-	if (i != idx)
-		return (NULL);
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	new_node->n = n;
+	if (h == NULL || new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	temp = *h;
+	for (i = 0; i < idx && temp; i++)
+		temp = temp->next;
 	if (idx == 0)
 	{
 		new_node->next = *h;
-		new_node->prev = NULL;
-		(*h)->next = new_node;
+		if (*h)
+			(*h)->next = new_node;
 		return (new_node);
 	}
-	new_node->next = temp->next;
-	new_node->prev = temp;
-	if (temp->next != NULL)
-		temp->next->prev = new_node;
-	temp->next = new_node;
-	return (new_node);
+	if (i != idx)
+	{
+		free(new_node);
+		return NULL;
+	}
+	if (!temp)
+	{
+		temp = *h;
+		while (temp->next)
+			temp = temp->next;
+		new_node->prev = temp;
+		temp->next = new_node;
+		return new_node;
+	}
+	new_node->next = temp;
+	new_node->prev = temp->prev;
+	if (temp->prev)
+		temp->prev->next = new_node;
+	temp->prev = new_node;
+	return new_node;
 }
